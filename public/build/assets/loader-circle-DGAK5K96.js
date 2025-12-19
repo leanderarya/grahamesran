d66826a068a1190b0628c64af39ecced5335d849
leanderarya <aryaajisadda@gmail.com>
2025-12-19 23:49:04 +0700

    NPM Run Build

--- public/build/assets/loader-circle-DGAK5K96.js
+++ public/build/assets/loader-circle-DGAK5K96.js
@@ -1,4 +1,4 @@
-import{a as e}from"./app-logo-icon-NZMsEDCo.js";/**
+import{b as e}from"./app-logo-icon-Cp1NQ0hw.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
