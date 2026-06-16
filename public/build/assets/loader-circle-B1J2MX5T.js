3eba6b418b5f6b762af6448a4b6e960551473616
leanderarya <aryaajisadda@gmail.com>
2026-06-16 14:40:58 +0700

    build: compile with design system tokens and components

--- public/build/assets/loader-circle-B1J2MX5T.js
+++ public/build/assets/loader-circle-B1J2MX5T.js
@@ -1,4 +1,4 @@
-import{c as e}from"./app-logo-icon-3shlpzlv.js";/**
+import{c as e}from"./createLucideIcon-DOYsFUBb.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
